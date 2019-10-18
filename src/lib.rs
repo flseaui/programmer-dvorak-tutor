856f142e03b6da9fb7148bde424ffd90d8d2101f
Claire Meyer <signedrewind@gmail.com>
2019-10-17 20:54:38 -0700

    Another restructuring, multi threaded lesson running (going to redo)

--- src/lib.rs
+++ src/lib.rs
@@ -5,11 +5,17 @@ extern crate termion;
 use clap::ErrorKind;
 use colored::Colorize;
 use std::borrow::{Borrow, BorrowMut};
-use std::cell::Cell;
+use std::cell::{Cell, RefCell};
 use std::fs::read_to_string;
-use std::io::{stdin, stdout, Error, Read, Stdin, Stdout, StdoutLock, Write};
+use std::io::{stdin, stdout, Error, Read, Stdin, StdinLock, Stdout, StdoutLock, Write};
 use std::ops::Deref;
+use std::rc::Rc;
 use std::str::Chars;
+use std::sync::mpsc::{Receiver, Sender};
+use std::sync::Mutex;
+use std::sync::{mpsc, Arc};
+use std::thread;
+use std::thread::{JoinHandle, Thread};
 use termion::cursor::DetectCursorPos;
 use termion::event::Key;
 use termion::event::Key::Char;
@@ -20,25 +26,11 @@ pub struct Lesson {
     text: String,
 }
 
+unsafe impl Send for Lesson {}
+
 impl Lesson {
     fn new(text: String) -> Lesson {
         Lesson { text }
-    }
-}
-
-pub struct Session {
-    stdout: RawTerminal<Stdout>,
-    stdin: Stdin,
-    lesson: Cell<Option<Lesson>>,
-}
-
-impl Session {
-    pub fn new(stdout: RawTerminal<Stdout>, stdin: Stdin) -> Session {
-        Session {
-            stdout,
-            stdin,
-            lesson: Cell::new(Some(Lesson::new(String::from("TEST")))),
-        }
     }
 }
 
@@ -46,30 +38,31 @@ pub fn create_app() {
     let yaml = load_yaml!("../cli.yml");
     let matches = clap::App::from(yaml).get_matches();
 
-    let session = start_session();
+    let (tx, rx) = mpsc::channel();
+
+    let handle = run(rx);
 
     if matches.is_present("lesson") {
         let lesson_str = matches.value_of("lesson").unwrap();
-        session.lesson.set(Some(load_lesson(lesson_str)));
+        tx.send(load_lesson(lesson_str))
+            .expect("Lesson message sending failed!");
     }
+
+    handle.join();
 }
 
-fn start_session() -> &'static Session {
-    let stdout = stdout().into_raw_mode().unwrap();
-    let stdin = stdin();
+fn run(rx: mpsc::Receiver<Lesson>) -> JoinHandle<()> {
+    let lesson_runner = thread::spawn(move || loop {
+        for lesson in &rx {
+            let again = run_lesson(&lesson);
+            if again {
 
-    let session = Session::new(stdout, stdin);
-
-    &session;
-
-    loop {
-        match &session.lesson.take() {
-            Some(lesson) => {
-                let again = run_lesson(&lesson, &session);
+            } else {
+                //return;
             }
-            _ => {}
         }
-    }
+    });
+    lesson_runner
 }
 
 fn load_lesson(lesson: &str) -> Lesson {
@@ -79,9 +72,13 @@ fn load_lesson(lesson: &str) -> Lesson {
     Lesson::new(lesson_string)
 }
 
-pub fn run_lesson(lesson: &Lesson, session: &Session) -> bool {
-    let stdin = &mut session.stdin.lock();
-    let stdout = &mut session.stdout.lock();
+pub fn run_lesson(lesson: &Lesson) -> bool {
+    let raw_stdout = stdout().into_raw_mode().unwrap();
+    let raw_stdin = stdin();
+
+    let mut stdout = raw_stdout.lock();
+    let mut stdin = raw_stdin.lock();
+
     let (mut cursor_x, mut cursor_y) = stdout.cursor_pos().unwrap();
 
     fn update_cursor_pos(cursor_x: &mut u16, cursor_y: &mut u16, stdout: &mut StdoutLock) {
@@ -96,7 +93,7 @@ pub fn run_lesson(lesson: &Lesson, session: &Session) -> bool {
         termion::clear::All,
         termion::cursor::Goto(1, 1)
     );
-    update_cursor_pos(&mut cursor_x, &mut cursor_y, stdout);
+    update_cursor_pos(&mut cursor_x, &mut cursor_y, &mut stdout);
 
     let top_offset = 1;
     let first_line = top_offset + 1;
@@ -104,6 +101,7 @@ pub fn run_lesson(lesson: &Lesson, session: &Session) -> bool {
     let mut lines = lesson.text.lines();
     let title = lines.next().unwrap();
     writeln!(stdout, "{}", title);
+
     for line in lines {
         write!(
             stdout,
@@ -127,15 +125,16 @@ pub fn run_lesson(lesson: &Lesson, session: &Session) -> bool {
             )
         );
         stdout.flush().unwrap();
-        update_cursor_pos(&mut cursor_x, &mut cursor_y, stdout);
+        update_cursor_pos(&mut cursor_x, &mut cursor_y, &mut stdout);
 
         let mut line_chars = line.chars();
         let line_length = line_chars.clone().count() as u16;
         'character: loop {
-            for c in stdin.keys() {
+            let mut keys = stdin.by_ref().keys();
+            for c in keys {
                 match c.unwrap() {
                     Key::Char(c) => {
-                        if cursor_x == line_length + 1 {
+                        if cursor_x >= line_length + 1 {
                             if c == '\n' {
                                 break 'character;
                             }
@@ -172,7 +171,7 @@ pub fn run_lesson(lesson: &Lesson, session: &Session) -> bool {
                     }
                 }
 
-                update_cursor_pos(&mut cursor_x, &mut cursor_y, stdout);
+                update_cursor_pos(&mut cursor_x, &mut cursor_y, &mut stdout);
                 stdout.flush().unwrap();
             }
         }
@@ -181,25 +180,47 @@ pub fn run_lesson(lesson: &Lesson, session: &Session) -> bool {
     // Lesson finished
     write!(
         stdout,
-        "{}{} finished, next lesson? (y/n) ",
+        "{}{} finished, next lesson? (y/n) {}",
         termion::cursor::Goto(1, cursor_y + 1),
-        title
+        title,
+        termion::cursor::Right(1)
     );
-
+    stdout.flush().unwrap();
+    update_cursor_pos(&mut cursor_x, &mut cursor_y, &mut stdout);
     // true - yes, false - no
-    let answer: bool = match stdin.read_line() {
-        Ok(line) => match line {
-            None => true,
-            Some(answer) => {
-                if answer.eq_ignore_ascii_case("y") || answer.eq_ignore_ascii_case("yes") {
-                    true
-                } else {
-                    false
-                }
-            }
-        },
-        Err(_err) => false,
-    };
 
-    return answer;
+    let start_x = cursor_x.clone();
+    let mut answer = false;
+    'outer: loop {
+        for key in stdin.by_ref().keys() {
+            match key.unwrap() {
+                Key::Char(c) => {
+                    if c == '\n' {
+                        break 'outer;
+                    } else {
+                        let x = c.eq_ignore_ascii_case(&'n');
+                        let y = c.eq_ignore_ascii_case(&'y');
+                        answer = y;
+                        if x || y {
+                            if cursor_x != start_x + 1 {
+                                write!(
+                                    stdout,
+                                    "{}{}",
+                                    termion::cursor::Goto(cursor_x - 1, cursor_y),
+                                    c
+                                );
+                            } else {
+                                write!(stdout, "{}", c);
+                            }
+                        }
+                    }
+                }
+                _ => {}
+            }
+
+            stdout.flush().unwrap();
+        }
+    }
+
+    answer
 }
